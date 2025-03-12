bfa2da5b98927d2cbb6091c019e9d4cee26fdcbd
dancan <dancan.morara@student.moringaschool.com>
2025-03-12 11:03:01 +0300

    completed-lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = 'Dancan Morara';
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const upperCaseCurrentUser = currentUser.toUpperCase();
+let shortGreeting = 'Welcome, ' + currentUser[0] + '!';
